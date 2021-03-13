f162cf0c2ce5460be620ea00e5bae7de67d97659
LeandroSimo <leo.bt.12@hotmail.com>
2021-03-13 08:25:48 -0300

    repositories implementados

--- src/main/java/com/leandro/bookstore/domain/Livro.java
+++ src/main/java/com/leandro/bookstore/domain/Livro.java
@@ -2,12 +2,18 @@ package com.leandro.bookstore.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class Livro implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -15,6 +21,8 @@ public class Livro implements Serializable {
 	private String nome_autor;
 	private String texto;
 
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
