f48c3836f94239f6a5103b7dd0765a7ff7ba4da3
LeandroSimo <leo.bt.12@hotmail.com>
2022-05-08 23:27:43 -0300

    Creating Repositories

---
+++ src/main/java/com/leandro/bookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.leandro.bookstore.repository;
+
+import com.leandro.bookstore.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
---
+++ src/main/java/com/leandro/bookstore/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.leandro.bookstore.repository;
+
+import com.leandro.bookstore.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
